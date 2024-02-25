f096487acad231401e6c3df0cb1395bd106dc8f5
Hacker94 <83153199+Ketan-vscode@users.noreply.github.com>
2024-02-25 20:09:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+status = "";
+
 function setup(){
     canvas = createCanvas(380,380);
     canvas.position(500,285);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    objcetDetector = ml5.objcetDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+    input = document.getElementById("input1");
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
+
+function draw(){
+    image(video, 0,0,500,285)
 }
