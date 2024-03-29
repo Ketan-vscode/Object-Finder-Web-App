66934452226958fd82f61c3a6cb0f6be408691f9
Hacker94 <83153199+Ketan-vscode@users.noreply.github.com>
2024-03-29 23:42:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup(){
     canvas = createCanvas(380,380);
@@ -20,4 +21,40 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0,0,500,285)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            console.log(object.length);
+            fill('ff0000')
+            percent = (objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke('#ff0000')
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+            if(objects[i].label == input_text){
+                video.stop();
+                object_detector.detect(gotResult);
+                document.getElementById("found").innerHTML = input_text + "Found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(input_text + found);
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("found").innerHTML = input_text + "Not found"
+            }
+        }
+    }
 }
